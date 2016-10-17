4ac690b6cf31386276074844109896af8d5cd9d3
jpcurrier <email@jackpaynecurrier.com>
2016-10-17 14:34:10 -0400

    add grouping

--- simple-overlay/simple-overlay.js
+++ simple-overlay/simple-overlay.js
@@ -1,82 +1,130 @@
-$( window ).on( 'resize', function(){
-	if( $( '.overlay' ).length )
-		overlaySizing();
-} );
-
-// overlay
-function overlaySizing(){
-	var figurePadH =
-		parseInt( $( '.overlay figure' ).css( 'padding-right' ), 10 ) +
-		parseInt( $( '.overlay figure' ).css( 'padding-left' ), 10 );
-	var figurePadV =
-		parseInt( $( '.overlay figure' ).css( 'padding-top' ), 10 ) +
-		parseInt( $( '.overlay figure' ).css( 'padding-bottom' ), 10 );
-	var overlayWidth =
-		$( window ).width() - (
-			parseInt( $( '.overlay' ).css( 'padding-right' ), 10 ) +
-			parseInt( $( '.overlay' ).css( 'padding-left' ), 10 ) +
-			figurePadH
-		);
-	var overlayHeight =
-		$( window ).height() - (
-			parseInt( $( '.overlay' ).css( 'padding-top' ), 10 ) +
-			parseInt( $( '.overlay' ).css( 'padding-bottom' ), 10 ) +
-			figurePadV
-		);
-
-	$( '.overlay' ).each( function(){
-		var figureWidth = Number( $( this ).find( 'figure' ).attr( 'data-natural-width' ) );
-		var figureHeight = Number( $( this ).find( 'figure' ).attr( 'data-natural-height' ) );
-
-		// figure width extends beyond overlay & extends further than figure height
-		if(
-			( overlayWidth - figureWidth < 0 ) &&
-			( overlayWidth - figureWidth < overlayHeight - figureHeight )
-		){
-			$( this ).find( 'figure' ).css({
-				'width': '100%',
-				'height': figureHeight * ( overlayWidth / figureWidth ) + figurePadV
-			});
-		}
-		// figure height extends beyond overlay, further than figure width
-		else if( overlayHeight - figureHeight < 0 ){
-			$( this ).find( 'figure' ).css({
-				'width': figureWidth * ( overlayHeight / figureHeight ) + figurePadH,
-				'height': '100%'
-			});
-		}
-		// figure does not extend beyond overlay
-		else{
-			$( this ).find( 'figure' ).css({
-				'width': figureWidth + figurePadH,
-				'height': figureHeight + figurePadV
-			});
-		}
-	} );
-}
+/*! Simple Overlay 0.1.0 | MIT *
+ * https://github.com/jpcurrier/simple-overlay !*/
 ( function( $ ){
-	$.fn.overlay = function( options ){
+	$.fn.simpleOverlay = function(){
+		// overlay background
+		if( !$( '.simple-overlay-bg' ).length )
+			$( 'body' ).prepend( '<div class="simple-overlay-bg"></div>' );
 
-		// default options
-		var settings = $.extend({
-			open: 'href'
-		}, options );
+		function overlaySizing(){
+			// dimensions
+			var $overlay = $( '.simple-overlay' ),
+				$figure = $( '.simple-overlay figure' ),
+				figurePadH = parseInt( $figure.css( 'padding-right' ) ) + parseInt( $figure.css( 'padding-left' ) ),
+				figurePadV = parseInt( $figure.css( 'padding-top' ) ) + parseInt( $figure.css( 'padding-bottom' ) ),
+				overlayWidth = $( window ).width() - parseInt( $overlay.css( 'padding-right' ) ) - parseInt( $overlay.css( 'padding-left' ) ) - figurePadH,
+				overlayHeight = $( window ).height() - parseInt( $overlay.css( 'padding-top' ) ) - parseInt( $overlay.css( 'padding-bottom' ) ) - figurePadV;
+
+			$( '.simple-overlay' ).each( function(){
+				var naturalWidth = Number( $( this ).find( 'figure' ).attr( 'data-natural-width' ) ),
+					naturalHeight = Number( $( this ).find( 'figure' ).attr( 'data-natural-height' ) );
+
+				// figure width extends beyond overlay & extends further than figure height
+				if(
+					( overlayWidth - naturalWidth < 0 ) &&
+					( overlayWidth - naturalWidth < overlayHeight - naturalHeight )
+				){
+					$( this ).find( 'figure' ).css({
+						'width': '100%',
+						'height': naturalHeight * ( overlayWidth / naturalWidth ) + figurePadV
+					});
+				}
+				// figure height extends beyond overlay, further than figure width
+				else if( overlayHeight - naturalHeight < 0 ){
+					$( this ).find( 'figure' ).css({
+						'width': naturalWidth * ( overlayHeight / naturalHeight ) + figurePadH,
+						'height': '100%'
+					});
+				}
+				// figure does not extend beyond overlay
+				else{
+					$( this ).find( 'figure' ).css({
+						'width': naturalWidth + figurePadH,
+						'height': naturalHeight + figurePadV
+					});
+				}
+			} );
+		}
+		$( window ).on( 'resize', function(){
+			overlaySizing();
+		} );
+
+		function closeSlide(){
+			$( '.simple-overlay.on' ).removeClass( 'on' ).fadeOut( 200 );
+		}
+		function openOverlay( overlayID ){
+			$( '.simple-overlay[ data-overlay="' + overlayID +'" ]' ).css({
+				'display': 'block',
+				'opacity': 0
+			}).addClass( 'on' );
+			overlaySizing();
+			$( '.simple-overlay[ data-overlay="' + overlayID +'" ]' ).animate({
+				'opacity': 1
+			});
+		}
+
+		// group navigation
+		$( document ).on( 'click', '.simple-overlay .prev, .simple-overlay .next', function( e ){
+			var $current = $( '.simple-overlay.on' ),
+				direction = $( e.currentTarget ).attr( 'class' );
+
+			var group = [];
+			$( '.simple-overlay[ data-group="' + $( this ).closest( '.simple-overlay' ).attr( 'data-group' ) + '" ]' ).each( function(){
+				group.push( {
+					element: $( this ),
+					i: $( this ).attr( 'data-overlay' )
+				} );
+			} );
+			group.sort( function( a, b ){
+				return a.i - b.i;
+			} );
+
+			var slidePos = false;
+			for( var slide in group ){
+				if( group[ slide ].i === $current.attr( 'data-overlay' ) )
+					slidePos = Number( slide );
+			}
+			if( slidePos !== false ){
+				closeSlide();
+				if( direction === 'prev' ){
+					if( slidePos !== 0 )
+						openOverlay( group[ slidePos - 1 ].element.attr( 'data-overlay' ) );
+					else
+						openOverlay( group[ group.length - 1 ].element.attr( 'data-overlay' ) );
+				}
+				if( direction === 'next' ){
+					if( $current.attr( 'data-overlay' ) < group.length )
+						openOverlay( group[ slidePos + 1 ].element.attr( 'data-overlay' ) );
+					else
+						openOverlay( group[ 0 ].element.attr( 'data-overlay' ) );
+				}
+			}
+		} );
 
 		var id = 1;
-
 		return this.each( function(){
-			var overlay = 'overlay-' + id,
-				src = $( this ).attr( 'href' );
+			var overlayID = id,
+				src = $( this ).attr( 'href' ),
+				group =
+					$( this ).attr( 'data-group' ) ?
+						' data-group="' + $( this ).attr( 'data-group' ) + '"' :
+							'',
+				groupClass =
+					group ?
+						' grouped' :
+							'';
 
-			// create
+			// create overlays
 			var img = new Image();
 			img.onload = function(){
 				$( 'body' ).append(
-					'<div class="overlay ' + overlay + '">' +
+					'<div class="simple-overlay' + groupClass + '" data-overlay="' + overlayID + '"' + group + '>' +
 						'<div>' +
 							'<figure data-natural-width="' + this.width + '" data-natural-height="' + this.height + '">' +
 								'<img src="' + src + '">' +
-								'<button class="close" type="button"></button>' +
+								'<button class="prev"><div></div></button>' +
+								'<button class="next"><div></div></button>' +
+								'<button class="close"></button>' +
 							'</figure>' +
 						'</div>' +
 					'</div>'
@@ -88,28 +136,23 @@ function overlaySizing(){
 			$( this ).on( 'click', function( e ){
 				e.preventDefault();
 
-				$( 'body' ).addClass( 'overlay-open' );
-
-				$( '.' + overlay ).css({
-					'display': 'block',
-					'opacity': 0
-				});
-				overlaySizing();
-				$( '.' + overlay ).animate({
-					'opacity': 1
-				});
-
+				$( 'body' ).addClass( 'simple-overlay-open' );
+				$( '.simple-overlay-bg' ).fadeIn( 200 );
+				setTimeout( function(){
+					openOverlay( overlayID );
+				}, 100 );
 			} );
 
 			// close
 			function closeOverlay(){
-				$( 'body' ).removeClass( 'overlay-open' );
-				$( '.overlay' ).fadeOut( 200 );
+				$( 'body' ).removeClass( 'simple-overlay-open' );
+				$( '.simple-overlay-bg, .simple-overlay' ).fadeOut( 200 );
+				$( '.simple-overlay.on' ).removeClass( 'on' );
 			}
-			$( document ).on( 'click', '.overlay figure', function( e ){
+			$( document ).on( 'click', '.simple-overlay figure', function( e ){
 				e.stopPropagation();
 			} );
-			$( document ).on( 'click', '.overlay, .overlay .close', function(){
+			$( document ).on( 'click', '.simple-overlay, .simple-overlay .close', function(){
 				closeOverlay();
 			} );
 			$( document ).on( 'keydown', function( e ){
@@ -122,6 +165,3 @@ function overlaySizing(){
 		} );
 	};
 } )( jQuery );
-
-// init overlay
-$( '.launch-overlay > a, .launch-overlay' ).overlay();
